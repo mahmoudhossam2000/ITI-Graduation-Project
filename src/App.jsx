6921b60024e39bcf15d4878623b47e4ab7f7214a
Mohamed Nooh <noohmohamed1999@gmail.com>
2025-07-21 18:07:39 +0300

    add dashboard to display comlaints

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import ComplaintForm from "./Components/ComplaintForm";
 import ComplaintSearch from "./Components/ComplaintSearch";
+import Layout from "./Components/features/authority_Dashboard/Layout";
+import Dashboard from "./Pages/Dashboard";
+import Complaints from "./Pages/Complaints";
 
 function App() {
   return (
@@ -13,6 +16,10 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/submitComplaint" element={<ComplaintForm />} />
           <Route path="/traceComplaint" element={<ComplaintSearch />} />
+          <Route path="/dashboard" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="complaints" element={<Complaints />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
